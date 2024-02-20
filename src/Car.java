7fe5122a0e3f4c967d564c43b406efadfd77bd1f
adamd <adam.d.hinton@gmail.com>
2024-02-20 01:00:14 -0500

    add fields to Car class

--- src/Car.java
+++ src/Car.java
@@ -1,2 +1,14 @@
+
+// This class is like a blueprint for an object we'll create.
+// Public means it can be used anywhere.
 public class Car {
+
+    // called fields because they're defined in a class's body and not in a method
+    // Much like defining interface properties in TS
+  private String make;
+  private String model;
+  private String color;
+  private int doors;
+  private boolean convertible;
+
 }
